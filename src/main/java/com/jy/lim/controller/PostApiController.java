20f87c84b2005ac69a04052206e4a29266524db9
sazzeo <imjee02@gmail.com>
2022-05-16 15:13:36 +0900

    add pageable , test package

---
+++ src/main/java/com/jy/lim/controller/PostApiController.java
@@ -0,0 +1,19 @@
+package com.jy.lim.controller;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PostApiController {
+
+    @GetMapping("/post")
+    public void findPost(@PageableDefault(size=20 , sort="" , direction = Sort.Direction.DESC) Pageable pageable) {
+
+        //finaAll 변수로 pageAll지정함.
+
+    }
+
+}
