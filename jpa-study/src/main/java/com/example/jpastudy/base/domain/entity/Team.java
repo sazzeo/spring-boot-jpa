5d2d3df15a24403b56d3c8cd2345126d22ab2ac4
jiyeong-dt <imjee02@gmail.com>
2024-04-06 06:46:12 +0900

    feat: 양방향 연관 관계 테스트 추가
    - 양방향_연관관계를_저장한다
    - 주인이_아닌쪽은_연관관계를_설정하지_못한다

--- jpa-study/src/main/java/com/example/jpastudy/base/domain/entity/Team.java
+++ jpa-study/src/main/java/com/example/jpastudy/base/domain/entity/Team.java
@@ -3,6 +3,8 @@ package com.example.jpastudy.base.domain.entity;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Table(name = "team")
 @AllArgsConstructor
@@ -19,5 +21,9 @@ public class Team {
     private Long teamId;
 
     private String name;
+
+    @OneToMany(mappedBy = "team")
+    @Builder.Default
+    private List<Member> members = new ArrayList<>();
 
 }
--- jpa-study/src/test/java/com/example/jpastudy/entity/MappingTest.java
+++ jpa-study/src/test/java/com/example/jpastudy/entity/MappingTest.java
@@ -2,21 +2,22 @@ package com.example.jpastudy.entity;
 
 import com.example.jpastudy.base.domain.entity.Member;
 import com.example.jpastudy.base.domain.entity.Team;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class MappingTest {
 
     static EntityManagerFactory emf;
     EntityManager entityManager;
-
     EntityManager entityManager2;
 
     @BeforeAll
@@ -86,7 +87,7 @@ public class MappingTest {
         transaction.commit();
 
         Member findedMember = entityManager2.find(Member.class, member.getId());
-        Assertions.assertThat(findedMember.getTeam().getName()).isEqualTo("팀2");
+        assertThat(findedMember.getTeam().getName()).isEqualTo("팀2");
     }
 
     @Test
@@ -110,7 +111,74 @@ public class MappingTest {
         transaction.commit();
 
         Member findedMember = entityManager2.find(Member.class, member.getId());
-        Assertions.assertThat(findedMember.getTeam()).isNull();
+        assertThat(findedMember.getTeam()).isNull();
+    }
+
+    @Test
+    void 양방향_연관관계를_저장한다() {
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+
+        //팀1 저장
+        Team team1 = Team.builder()
+                .name("팀1")
+                .build();
+        entityManager.persist(team1);
+
+        //멤버 1 저장
+        Member member1 = Member.builder()
+                .username("지영")
+                .build();
+        member1.setTeam(team1);
+        entityManager.persist(member1);
+
+        Member member2 = Member.builder()
+                .username("수영")
+                .build();
+        member2.setTeam(team1);
+        entityManager.persist(member2);
+
+        transaction.commit();
+    }
+
+    @Test
+    void 주인이_아닌쪽은_연관관계를_설정하지_못한다() {
+
+        transactional(entityManager, () -> {
+            //멤버 1 저장
+            Member member1 = Member.builder()
+                    .username("지영")
+                    .build();
+            entityManager.persist(member1);
+
+            Member member2 = Member.builder()
+                    .username("수영")
+                    .build();
+            entityManager.persist(member2);
+
+            //팀1 저장
+            Team team1 = Team.builder()
+                    .name("팀1")
+                    .build();
+            team1.getMembers().add(member1);
+            team1.getMembers().add(member2);
+            entityManager.persist(team1);
+            entityManager.flush();
+            entityManager.clear();
+
+            Member member = entityManager.find(Member.class, member1.getId());
+            assertThat(member.getTeam()).isNull();
+        });
+
+    }
+
+    void transactional(EntityManager entityManager, Runnable runnable) {
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+
+        runnable.run();
+
+        transaction.commit();
     }
 
 }
